64dad0c437babe21243cde40689096a9ca33106d
mmizani <maryam.mizani@gmail.com>
2018-12-08 06:48:48 -0500

    Add more info (description, price, .. ) when adding a product

--- routes/admin.js
+++ routes/admin.js
@@ -13,7 +13,7 @@ router.get('/add-product', adminController.getAddProduct);
 router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/admin-products', adminController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
 
