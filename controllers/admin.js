4a18d0ea974f229fd4aa60c98990b5aea36db72c
mmizani <maryam.mizani@gmail.com>
2018-12-09 08:22:07 -0500

    Add Edit product Page

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,12 +2,10 @@ const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('admin/add-product', {
+    res.render('admin/edit-product', {
       pageTitle: 'Add Product',
       path: '/admin/add-product',
-      activeAddProduct: true,
-      productCSS: true,
-      formsCSS: true
+      editing: false
     });
   };
 
@@ -19,6 +17,26 @@ exports.postAddProduct = (req, res, next) => {
     const product = new Product(title, imageUrl, description, price);
     product.save();
     res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;  // The return value is always String
+                                      // "true" instead of true
+    if(!editMode) {   
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if(!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+          });
+    })  
 };
 
 exports.getProducts = (req, res, next) => 
