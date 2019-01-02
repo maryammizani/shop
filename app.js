d2c66f3caf5f1c6fe3b49d0cfc22a8e1fd3925c8
mmizani <maryam.mizani@gmail.com>
2019-01-02 06:23:11 -0500

    Add a product to the Cart (Cart is inside the User model)

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById("5c2c1690f3933e3a30b6e837")
     .then(user => {
-        req.user = user;  
+        req.user = new User(user.name, user.email, user.cart, user._id);  
         next();
     })
     .catch(err => console.log(err));
