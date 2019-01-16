d679a9f45c3e503d96edf77f5b4f2c6a3636dc3a
mmizani <maryam.mizani@gmail.com>
2019-01-15 21:02:45 -0500

    SendEmail: send an email after user is signed in

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,16 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+const sendGridKey = require('../key').SENDGRID_KEY;
+
 const User = require('../models/user');
+
+const transport = nodemailer.createTransport(sendgridTransport({
+    auth: {
+        //api_user: ,
+        api_key: sendGridKey
+    }
+}));
 
 exports.getLogin = (req, res, next) => {
     let message = req.flash('error');
@@ -83,8 +94,18 @@ exports.postSignup = (req, res, next) => {
             return user.save();
         })
         .then(result => {
-            res.redirect('/login')
-        })     
+            res.redirect('/login');
+            // Sending emials can be slow, so do not use it in a blocking way if you have too many users
+            transport.sendMail({
+                to: email,
+                from: 'shop@node-complete.com',
+                subject: 'Signup succeesed!',
+                html: '<h1>You successfully signed up!</h1>'
+            })
+            .catch(err => {
+                console.log(err);
+            });          
+        });     
     })  
     .catch(err => {
         console.log(err);
