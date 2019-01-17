d88ddcf0ded79bf434c4b9a7f2af9ea7ac8c2e07
mmizani <maryam.mizani@gmail.com>
2019-01-17 05:00:25 -0500

    Auth: Create a token to reset the password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,9 @@
+const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 const sendGridKey = require('../key').SENDGRID_KEY;
+const URL = require('../key').URL;
 
 const User = require('../models/user');
 
@@ -133,3 +135,41 @@ exports.getReset = (req, res, next) => {
         errorMessage: message
     });
 };
+
+exports.postReset = (req, res, next) => {
+    crypto.randomBytes(32, (err, buffer) => {  // generates 32 random bytes
+        if(err) {
+            console.log(err);
+            return res.redirect('/reset');
+        }
+        const token = buffer.toString('hex');
+        User.findOne({email: req.body.email})
+        .then(user => {
+            if(!user) {
+                req.flash('error', 'No account with that email found.');
+                return res.redirect('/reset');
+            }     
+            user.resetToken = token;
+            user.resetTokenExpiration = Date.now() + 3600000; // 3,600,000 milliseconds, = 1 hour
+            user.save()
+            .then(result => {
+                res.redirect('/');
+                transport.sendMail({
+                    to: req.body.email,
+                    from: 'shop@node-complete.com',
+                    subject: 'Password reset',  
+                    html: ` 
+                        <p>You requested a password reset</p>
+                        <p>Click this <a href="${URL}/reset/${token}"link</a> to set a new password.</p>
+                    `
+                })
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        })      
+        .catch(err => {
+            console.log(err);
+        });
+    });
+}
