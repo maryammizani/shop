d996bcf2019406c2bb7e17771cb30ad77d272224
mmizani <maryam.mizani@gmail.com>
2019-02-12 05:53:12 -0500

    File Upload: Download Invoice Files

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -159,3 +161,16 @@ exports.getCheckout = (req, res, next) => {
             path:'/checkout',
           });
 };
+
+exports.getInvoice = (req, res, next) => {
+    const orderId = req.params.orderId;
+    const invoiceName = 'invoice-' + orderId + '.pdf';
+    const invoicePath = path.join('data', 'invoices', invoiceName);
+
+    fs.readFile(invoicePath, (err, data) => {
+        if(err) {
+            return next(err);
+        }
+        res.send(data);
+    })
+}
