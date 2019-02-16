8e8c65196b61733b8aa20ee88886b8d83b128f68
mmizani <maryam.mizani@gmail.com>
2019-02-16 08:56:21 -0500

    Add Pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,15 +5,34 @@ const PDFDocument = require('pdfkit'); // used to create pdf files (invoice)
 const Product = require('../models/product');
 const Order = require('../models/order');
 
+const ITEMS_PER_PAGE = 1;
+
 exports.getProducts = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems;
     Product.find()
+    .countDocuments()
+    .then(numProducts => {
+        totalItems = numProducts;
+        return Product.find()
+        .skip((page -1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
         res.render('shop/product-list', {
             prods: products, 
-            pageTitle: 'All Products', 
-            path:'/products',
+            pageTitle: 'Products', 
+            path:'products',
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page -1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+            // isAuthenticated: req.session.isLoggedIn,
+            // csrfToken: req.csrfToken()
           });
-        })
+    })
     .catch(err => {
         const error = new Error(err);
         error.httpStatusCode = 500;
@@ -39,16 +58,31 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
+    const page = +req.query.page || 1;
+    let totalItems;
     Product.find()
+    .countDocuments()
+    .then(numProducts => {
+        totalItems = numProducts;
+        return Product.find()
+        .skip((page -1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
         res.render('shop/index', {
             prods: products, 
             pageTitle: 'Shop', 
-            path:'/'
+            path:'/',
+            currentPage: page,
+            hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+            hasPreviousPage: page > 1,
+            nextPage: page + 1,
+            previousPage: page -1,
+            lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             // isAuthenticated: req.session.isLoggedIn,
             // csrfToken: req.csrfToken()
           });
-        })
+    })
     .catch(err => {
         const error = new Error(err);
         error.httpStatusCode = 500;
